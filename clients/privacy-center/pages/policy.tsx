7e1d0e587430e347d359551a633ea457abca05ab
Sean Lawlor <95254483+spacesean@users.noreply.github.com>
2024-04-11 10:42:40 -0700

    fix lint issues

--- clients/privacy-center/pages/policy.tsx
+++ clients/privacy-center/pages/policy.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import type { NextPage } from "next";
-import Head from "next/head";
 import { Stack } from "@fidesui/react";
 
-const PrivacyPolicy: NextPage = () => {
-  return (
-    <main data-testid="home">
-      <Stack align="center" py={["6", "16"]} px={5} spacing={14}>
-        <iframe
-          src="https://legal.snackpass.co/snackpass-privacy-policy"
-          title="Snackpass Privacy Policy"
-        />
-      </Stack>
-    </main>
-  );
-};
+const PrivacyPolicy: NextPage = () => (
+  <main data-testid="home">
+    <Stack align="center" py={["6", "16"]} px={5} spacing={14}>
+      <iframe
+        src="https://legal.snackpass.co/snackpass-privacy-policy"
+        title="Snackpass Privacy Policy"
+      />
+    </Stack>
+  </main>
+);
 
 export default PrivacyPolicy;
