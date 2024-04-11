351b6c162f2742b9b4bdfbe947375bd50d40fa8a
Sean Lawlor <95254483+spacesean@users.noreply.github.com>
2024-04-11 10:45:43 -0700

    undo the header added to index

--- clients/privacy-center/pages/index.tsx
+++ clients/privacy-center/pages/index.tsx
@@ -167,92 +167,65 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div>
-      <Head>
-        <title>Snackpass Privacy Center</title>
-        <meta name="description" content="Privacy Center" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <header>
-        <Flex
-          bg="gray.100"
-          minHeight={14}
-          p={1}
-          width="100%"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Image
-            src={config.logo_path}
-            margin="8px"
-            height="68px"
-            alt="Logo"
-            data-testid="logo"
-          />
-        </Flex>
-      </header>
-
-      <main
-        data-testid="home"
-        style={{ overflow: "auto", height: "100vh", paddingBottom: "20vh" }}
+    <main
+      data-testid="home"
+      style={{ overflow: "auto", height: "100vh", paddingBottom: "20vh" }}
+    >
+      <Stack
+        align="center"
+        py={["6", "16"]}
+        px={5}
+        spacing={14}
+        overflow="scroll"
       >
-        <Stack
-          align="center"
-          py={["6", "16"]}
-          px={5}
-          spacing={14}
-          overflow="scroll"
-        >
-          <Stack align="center" spacing={3}>
-            <Heading
-              fontSize={["3xl", "4xl"]}
-              color="gray.600"
-              fontWeight="semibold"
-              textAlign="center"
-              data-testid="heading"
-            >
-              {config.title}
-            </Heading>
-            <Text
-              fontSize={["small", "medium"]}
-              fontWeight="medium"
-              maxWidth={624}
-              textAlign="center"
-              color="gray.600"
-              data-testid="description"
-            >
-              {config.description}
-            </Text>
-          </Stack>
-          <Flex m={-2} flexDirection={["column", "column", "row"]}>
-            {content}
-          </Flex>
+        <Stack align="center" spacing={3}>
+          <Heading
+            fontSize={["3xl", "4xl"]}
+            color="gray.600"
+            fontWeight="semibold"
+            textAlign="center"
+            data-testid="heading"
+          >
+            {config.title}
+          </Heading>
+          <Text
+            fontSize={["small", "medium"]}
+            fontWeight="medium"
+            maxWidth={624}
+            textAlign="center"
+            color="gray.600"
+            data-testid="description"
+          >
+            {config.description}
+          </Text>
         </Stack>
-        <PrivacyRequestModal
-          isOpen={isPrivacyModalOpen}
-          onClose={onPrivacyModalClose}
-          openAction={openAction}
-          currentView={currentPrivacyModalView}
-          setCurrentView={setCurrentPrivacyModalView}
-          privacyRequestId={privacyRequestId}
-          setPrivacyRequestId={setPrivacyRequestId}
-          isVerificationRequired={isVerificationRequired}
-          successHandler={privacyModalSuccessHandler}
-        />
+        <Flex m={-2} flexDirection={["column", "column", "row"]}>
+          {content}
+        </Flex>
+      </Stack>
+      <PrivacyRequestModal
+        isOpen={isPrivacyModalOpen}
+        onClose={onPrivacyModalClose}
+        openAction={openAction}
+        currentView={currentPrivacyModalView}
+        setCurrentView={setCurrentPrivacyModalView}
+        privacyRequestId={privacyRequestId}
+        setPrivacyRequestId={setPrivacyRequestId}
+        isVerificationRequired={isVerificationRequired}
+        successHandler={privacyModalSuccessHandler}
+      />
 
-        <ConsentRequestModal
-          isOpen={isConsentModalOpen}
-          onClose={onConsentModalClose}
-          currentView={currentConsentModalView}
-          setCurrentView={setCurrentConsentModalView}
-          consentRequestId={consentRequestId}
-          setConsentRequestId={setConsentRequestId}
-          isVerificationRequired={isVerificationRequired}
-          successHandler={consentModalSuccessHandler}
-        />
-      </main>
-    </div>
+      <ConsentRequestModal
+        isOpen={isConsentModalOpen}
+        onClose={onConsentModalClose}
+        currentView={currentConsentModalView}
+        setCurrentView={setCurrentConsentModalView}
+        consentRequestId={consentRequestId}
+        setConsentRequestId={setConsentRequestId}
+        isVerificationRequired={isVerificationRequired}
+        successHandler={consentModalSuccessHandler}
+      />
+    </main>
   );
 };
 
